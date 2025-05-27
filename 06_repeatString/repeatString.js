8a1a0e7720c71374c4fb34cec0dff1a9d874e42d
Callum Preston <CallumCPreston@outlook.com>
2025-05-27 20:18:59 +1000

    Complete assignment 06 repeatString
    Completed assignment repeatString. Used a for loop and appended onto
    an empty string as it was a straightforward approach

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, numRepeats) {
+    let repeatedStr = ''
+    if(numRepeats >= 0)
+    {
+        for(let i = 0; i < numRepeats; i++)
+            {
+                repeatedStr += str;
+            }
 
+    }
+    else
+    {
+        repeatedStr += 'ERROR';
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
