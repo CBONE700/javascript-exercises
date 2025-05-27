bbcf6b055c561aa231342d6dcc69cda52d4cc864
Callum Preston <CallumCPreston@outlook.com>
2025-05-27 21:03:07 +1000

    Complete assignment 09 sumAll
    Completed assignment 09 sumAll using for loops for an O(n) solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) 
+{
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+    {
+        return 'ERROR';
+    }
+    let count = 0;
 
+    if (a < b)
+    {
+        for(a; a <= b; a++)
+            {
+                count += a;
+            }
+    }
+    else
+    {
+        for(b; b <= a; b++)
+            {
+                count += b;
+            }
+    }
+    return count;
 };
 
 // Do not edit below this line
