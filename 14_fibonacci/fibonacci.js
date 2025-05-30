780c025c8e79a3633354c6929167f590698c3ba9
Callum Preston <CallumCPreston@outlook.com>
2025-05-30 14:22:04 +1000

    Complete assignment 14 fibonacci
    Completed assignment 14 fibonacci by using the estimation equation.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    return Math.floor(((((1 + (5 ** (1/2)))/2)**num) - (((1 - (5 ** (1/2)))/2)**num))/(5 ** (1/2)));
 };
 
 // Do not edit below this line
