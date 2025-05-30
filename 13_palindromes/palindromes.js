57de6bd9a77f22686687ae241ce538292ccce00e
Callum Preston <CallumCPreston@outlook.com>
2025-05-30 14:09:05 +1000

    Complete assignment 13 palindromes
    Completed palindromes by cleaning the string and then iteratively
    checking each letter.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let i = 0;
+    let j = newStr.length - 1;
+    while (j > i)
+    {
+        if (newStr[i] != newStr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
